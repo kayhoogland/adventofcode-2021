ec785c43dc677ace3e7ef7a0e37b20e15618dbaf
kayhoogland <kay@floryn.com>
2021-12-09 19:57:07 +0100

    Prettify day 09

--- day_09.nim
+++ day_09.nim
@@ -1,8 +1,10 @@
 import strutils, sequtils, tables, std/algorithm, sets
 
-type Coordinate = tuple[x: int, y: int]
+type
+  Coordinate = tuple[x: int, y: int]
+  Map = Table[Coordinate, int]
 
-proc createMap(input: string): Table[Coordinate, int] =
+proc createMap(input: string): Map =
   for y, line in toSeq(input.lines):
     for x, number in line:
       result[(x, y)] = parseInt($number)
@@ -10,43 +12,37 @@ proc createMap(input: string): Table[Coordinate, int] =
 proc neighbors(c: Coordinate): seq[Coordinate] =
   return @[(c.x-1, c.y), (c.x+1, c.y), (c.x, c.y-1), (c.x, c.y+1)]
 
-proc isLowPoint(c: Coordinate, map: Table[Coordinate, int], keys: seq[
-    Coordinate]): bool =
+proc isLowPoint(c: Coordinate, map: Map): bool =
   result = true
   let value = map[c]
   for n in c.neighbors:
-    if n in keys and value >= map[n]:
+    if n in map and value >= map[n]:
       return false
 
-proc lowPoints(map: Table[Coordinate, int], keys: seq[Coordinate]): seq[Coordinate] =
-  return keys.filterIt(it.isLowPoint(map, keys))
+proc lowPoints(map: Map): seq[Coordinate] =
+  return toSeq(map.keys).filterIt(it.isLowPoint(map))
 
-proc higherNeighbors(c: Coordinate, map: Table[Coordinate, int], keys: seq[
-    Coordinate]): seq[Coordinate] =
-  return c.neighbors.filterIt(it in keys and map[it] > map[c] and map[it] != 9)
+proc higherNeighbors(c: Coordinate, map: Map): seq[Coordinate] =
+  return c.neighbors.filterIt(it in map and map[it] > map[c] and map[it] != 9)
 
-proc bassin(c: Coordinate, map: Table[Coordinate, int], keys: seq[
-    Coordinate]): HashSet[Coordinate] =
-  let higher_neighbors = c.higherNeighbors(map, keys)
+proc bassin(c: Coordinate, map: Map): HashSet[Coordinate] =
   result.incl(c)
-  if higher_neighbors.len > 0:
-    for hn in higher_neighbors:
-      result = result + hn.bassin(map, keys)
+  for hn in c.higherNeighbors(map):
+    result = result + hn.bassin(map)
 
-proc part1(map: Table[Coordinate, int], keys: seq[Coordinate]): int =
-  for c in map.lowPoints(keys):
+proc part1(map: Map): int =
+  for c in map.lowPoints:
     result += 1+map[c]
 
-proc part2(map: Table[Coordinate, int], keys: seq[Coordinate]): int =
+proc part2(map: Map): int =
   var sizes: seq[int] = @[]
-  for c in map.lowPoints(keys):
-    sizes.add(c.bassin(map, keys).len)
+  for c in map.lowPoints:
+    sizes.add(c.bassin(map).len)
   return sorted(sizes, Descending)[0..2].foldl(a*b)
 
 let
   input = "./input/day_09.txt"
   map = createMap(input)
-  keys = toSeq(map.keys)
 
-echo part1(map, keys)
-echo part2(map, keys)
+echo part1(map)
+echo part2(map)
